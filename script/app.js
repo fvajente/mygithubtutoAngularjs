b9fb8e086def440f43e884762ad86936eb3ccf97
Fabrice VAJENTE <fvajente@AVENCOD.local>
2017-03-14 08:59:03 +0100

    déplacement index dans dossier pages

---
+++ script/app.js
@@ -0,0 +1,21 @@
+/*(function () {
+'use strict';
+
+angular.module('PageCall', [])
+
+.config(function ($routeProvider) {
+   $routeProvider
+     .when('/page1', {
+       templateUrl: 'pages/page1.html',
+
+     })
+     .when('/page2', {
+       templateUrl: 'pages/page2.html',
+
+     })
+     .when('/page3', {
+       templateUrl: 'pages/page3.html',
+
+     })
+
+ });*/
